4cbee0268b188316e602ec9f8bc99dd1df52510c
pradeepcho-exe <102564484+pradeepcho-exe@users.noreply.github.com>
2022-05-27 12:07:07 -0500

    TipCalculator

---
+++ Day 2/TipCalculator.py
@@ -0,0 +1,7 @@
+print("Welcome the tip Calculator.")
+bill = input("What was the total bill?  $")
+tip = input("What percentage tip would you like to give? 10, 12, or 15?")
+people = input("How many people to split the bill?")
+each = float( (int(float(bill)) * (1 + (float(tip)/100)) ) / int(people) )
+each = round(each, 2)
+print(f"Each person should pay: ${each}")
